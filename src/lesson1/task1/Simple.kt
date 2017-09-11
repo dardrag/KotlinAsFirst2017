9cf67624154c0a2bb9a7bd83e97c025a0b555426
dardrag <windrag000@gmail.com>
2017-09-11 23:16:21 +0300

    angleInRadian

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -41,7 +41,7 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  */
 fun main(args: Array<String>) {
     // Решаем x^2 - 3*x + 2 = 0
-    val x1x2 = lengthInMeters(23,43,23)
+    val x1x2 = angleInRadian(36,14,35)
     println("Root product: $x1x2")
 }
 
@@ -70,7 +70,9 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double  {
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double {
+    return toRadians(grad + min / 60.0 + sec / 3600.0)
+}
 
 /**
  * Тривиальная
