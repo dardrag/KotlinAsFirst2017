cfaa5769b3416698c3dd7686d743b256e3033506
windrag <windrag000@gmail.com>
2017-12-04 03:06:56 +0300

    lesson 7 begin

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -39,6 +39,7 @@ interface Matrix<E> {
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
 fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (height <=0 || width <= 0) throw IllegalArgumentException()
     return MatrixImpl(height, width, e)
 }
 
